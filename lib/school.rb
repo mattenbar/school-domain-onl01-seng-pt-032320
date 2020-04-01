0270a09f9d345e893c32ff08fcce27de7a250d67
matthew enbar <matthew.enbar@gmail.com>
2020-04-01 14:59:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,14 +5,19 @@ attr_accessor
 attr_reader :roster, :add_student, :grade, :sort
 
   def initialize(roster)
+    #initializes with an empty roster hash
     @roster = {}
   end
 
   def add_student(name, grade)
+    #checks to see if grade key exist if not it creats an emoty array for its value
     roster[grade] ||= []
+    #pushes the name/ student into its grade
     roster[grade] << name
   end
 
+#returns all the values for the key grade
+#meaning its returns all the students in a certain grade
   def grade(grade)
     roster[grade]
   end
